b5b96e2f4279729fa0812ac13e05eb94116ff45d
cg2023269 <168424029+cg2023269@users.noreply.github.com>
2024-04-29 12:39:29 +0200

    Update random-text-generator.py

--- random-text-generator.py
+++ random-text-generator.py
@@ -6,7 +6,7 @@ def generar_texto_random(diccionario, num_palabras):
 
     texto_random = ' '.join(random.choices(palabras, k=num_palabras))
     return texto_random
-
+    cghvh
 diccionario = 'diccionario.txt'
 num_palabras = 100
 
