3d56a2a7bdf5613f400d483de2d98bc5dcdaa581
alexsansstucom <150898556+alexsansstucom@users.noreply.github.com>
2024-04-29 12:31:25 +0200

    Update random-text-generator.py

--- random-text-generator.py
+++ random-text-generator.py
@@ -1,3 +1,8 @@
 import random
 
-def
+def generar_text_random(diccionario, num_palabras):
+    with open(diccionario, 'r') as file:
+        palabras = file.read().split()
+
+    texto_random = ' '.join(random.choices(palabras, k=num_palabras))
+    return texto_random
