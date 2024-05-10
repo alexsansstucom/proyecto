c2adf4333ae740ebe15df9b5eb5b1a84003a805f
Gezetta <gonzalo.stucom@gmail.com>
2024-05-10 09:33:42 +0200

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,8 +1,21 @@
-class Atributos:
+class Ejercicio:
     def __init__(self):
+        self.resultados = []
 
-    def ejercicio():
+    def generar_ejercicio(self):
 
-    def resultados():
-        
-class Constructor:
+    def pulsacion(self):
+
+    def resultado(self):
+    
+class Usuario:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.historial_ejercicios = []
+        self.estadisticas = {}
+
+    def guardar_resultado(self):
+
+    def procesar_resultado(self):
+
+    def configurar_usuario(self):
