ea7a230090499d9d785f188671c4699ce99324c5
Gezetta <gonzalo.stucom@gmail.com>
2024-05-10 09:39:23 +0200

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,4 @@
+#Creación y correción de los ejercicios
 class Ejercicio:
     def __init__(self):
         self.resultados = []
@@ -7,7 +8,8 @@ class Ejercicio:
     def pulsacion(self):
 
     def resultado(self):
-    
+
+#Historial de usuario
 class Usuario:
     def __init__(self, nombre):
         self.nombre = nombre
@@ -15,7 +17,17 @@ class Usuario:
         self.estadisticas = {}
 
     def guardar_resultado(self):
-
+        pass
     def procesar_resultado(self):
-
+        pass
     def configurar_usuario(self):
+        pass
+
+#Interfaz
+class Interfaz:
+    def __init__(self):
+        pass
+    def menu_principal(self):
+        pass
+    def menu_configuracion(self):
+        pass
